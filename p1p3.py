d735d406a577626f6322634bea07f2bb4b6e51f2
ojhermann-ucd <otto.hermann@ucdconnect.ie>
2016-09-20 13:41:20 +0100

    Add files via upload

---
+++ p1p3.py
@@ -0,0 +1,5 @@
+print('Otto James Hermann')#this is my real name
+print('')#this will print a blank line
+print('1234 Fake Named Street, Apartment ABCD, Moscow, Russia')#this is my fake address
+print('')#this will print a blank line
+print('+1 800 123 4567')#this is my fake phone number
